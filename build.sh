353111a913cbfb3cf68bf95c9b4daaccbe58c867
Alula <6276139+alula@users.noreply.github.com>
2022-07-01 18:03:46 +0200

    build.sh for convenience

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+cd `dirname $0`
+
+OSNAME=`uname -s`
+
+if [ $OSNAME = "Linux" ]; then
+	echo "Building for Linux"
+
+	# https://github.com/godotengine/godot/issues/44532
+	mkdir -p ~/.local/share/godot/mono/GodotNuGetFallbackFolder
+
+	#scons -c
+	scons -j12 platform=linuxbsd tools=yes module_mono_enabled=yes mono_glue=no
+	./bin/godot.linuxbsd.tools.64.mono --generate-mono-glue modules/mono/glue/ 
+	scons -j12 platform=linuxbsd tools=yes module_mono_enabled=yes mono_glue=yes
+elif [ $OSNAME = "Darwin" ]; then
+	echo "Building for MacOS..."
+else
+	echo "Unknown OS: $OSNAME"
+	exit 1
+fi
